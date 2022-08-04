a369bd5d02c8146e8b42f92ae8c671c20e16b21d
Komilov Dilshod <komilovd@silkroadprofessionals.com>
2022-08-04 11:31:21 +0500

    Add project files.

---
+++ MyCalculator.cs
@@ -0,0 +1,39 @@
+﻿namespace Calculator
+{
+    public class MyCalculator
+    {
+        public string DivideTwoNumbers(string firstNumberStr, string secondNumberStr)
+        {
+            if (double.TryParse(firstNumberStr, out double firstNumber) && double.TryParse(secondNumberStr, out double secondNumber))
+            {
+                if (secondNumber == 0)
+                    return "Cannot divide to zero";
+                double result = firstNumber / secondNumber;
+                return $"Result of divide is {result}";
+            }
+            else
+            {
+                return "Parameters are not integer";
+            }
+        }
+
+        public string DivideTwoNumbersFromFile(string filePath, IFileReader fileReader)
+        {
+            var allLines = fileReader.GetLines(filePath);
+            return DivideTwoNumbers(allLines[0], allLines[1]);
+        }
+    }
+
+    public interface IFileReader
+    {
+        string[] GetLines(string filePath);
+    }
+
+    public class MyFileReader : IFileReader
+    {
+        public string[] GetLines(string filePath)
+        {
+            return File.ReadAllLines(filePath);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Calculator;
+
+MyCalculator myCalculator = new();
+MyFileReader myFileReader = new MyFileReader();
+while (true)
+{
+    Console.WriteLine("Enter file path");
+    var fileApth = Console.ReadLine();
+    Console.WriteLine(myCalculator.DivideTwoNumbersFromFile(fileApth, myFileReader));
+
+    //Console.WriteLine("Enter the first number");
+    //var firstNumber = Console.ReadLine();
+
+    //Console.WriteLine("Enter the second number");
+    //var secondNumber = Console.ReadLine();
+
+    //Console.WriteLine(myCalculator.DivideTwoNumbers(firstNumber, secondNumber));
+}
